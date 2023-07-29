21d02887279504355bd2ccdeab153752899cc9fe
devFesk <feskproject94@gmail.com>
2023-07-30 05:08:29 +0900

    예제 만들기

--- Study_CH_230729.py
+++ Study_CH_230729.py
@@ -1,1 +1,16 @@
-print('hello world')
+num = int(input())
+
+for i in range(0, num * 2 + 1):
+    if i < num:
+        for j in range(0, num - i):
+            print(" ", end = "")
+        print("*")
+    elif i == num:
+        for j in range(0, num + 1):
+            print("*", end = "")
+        print()
+    else:
+        k = i - num
+        for j in range(0, num - k):
+            print(" ", end = "")
+        print("*")
